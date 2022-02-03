772220bbc3f20c277cf0900369f95489eb342e6f
Volodymyr <mazurchak.volodymyr.work@gmail.com>
2022-02-03 13:13:17 +0200

    Comment out eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-    jquery: {
-      globals: {
-        $: true,
-      },
-    },
-  },
-  extends: ['airbnb-base', 'plugin:jest/recommended'],
-  plugins: ['jest'],
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  rules: {
-    'class-methods-use-this': 0,
-  },
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2020: true,
+//     node: true,
+//     jquery: {
+//       globals: {
+//         $: true,
+//       },
+//     },
+//   },
+//   extends: ['airbnb-base', 'plugin:jest/recommended'],
+//   plugins: ['jest'],
+//   parserOptions: {
+//     ecmaVersion: 11,
+//     sourceType: 'module',
+//   },
+//   rules: {
+//     'class-methods-use-this': 0,
+//   },
+// };
