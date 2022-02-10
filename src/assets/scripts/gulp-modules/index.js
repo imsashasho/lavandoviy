8e58692fb4d388993ce4e566fe21aedbc08b0f36
Mazurchak Volodymyr <mazurchak.volodymyr.work@gmail.com>
2022-02-10 12:17:19 +0200

    Complete call call-page

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -6,3 +6,19 @@ footerToTop.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+// Вызов меню
+const menuBtn = document.querySelector('.header-btn-menu');
+const callPage = document.querySelector('.call-page');
+const body = document.querySelector('body');
+const callPageCloseBtn = document.querySelector('.js-cp-close');
+
+menuBtn.addEventListener('click', () => {
+  callPage.classList.add('active');
+  body.classList.add('disabled-scroll');
+});
+
+callPageCloseBtn.addEventListener('click', () => {
+  callPage.classList.remove('active');
+  body.classList.remove('disabled-scroll');
+});
