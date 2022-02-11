cea8ae6375e3311cf62df3deaa4501bef1ca9a53
Mazurchak Volodymyr <mazurchak.volodymyr.work@gmail.com>
2022-02-11 15:56:19 +0200

    Complete progress-page

--- src/assets/scripts/gulp-modules/progress.js
+++ src/assets/scripts/gulp-modules/progress.js
@@ -1,6 +1,19 @@
-//Инициализация rangeSlider
+// Инициализация rangeSlider
 $('.js-range-slider').ionRangeSlider({
   type: 'single',
   min: 0,
   max: 100,
+});
+
+// Реализация появляющегося purple block
+$('.js-filter-text').on('click', event => {
+  $(event.target)
+    .siblings('.purple-block')
+    .addClass('active');
+});
+
+$('.js-purple-close').on('click', event => {
+  $(event.target)
+    .closest('.purple-block')
+    .removeClass('active');
 });
