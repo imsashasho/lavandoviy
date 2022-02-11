935d6075b2730b34694ba9d66ef1ba82dd5d4295
Volodymyr <mazurchak.volodymyr.work@gmail.com>
2022-02-11 03:33:27 +0200

    Complete transition to call-thanks-page

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -23,4 +23,45 @@ menuCloseBtn.addEventListener('click', () => {
   body.classList.remove('disabled-scroll');
 });
 
+// Вызов колл-пейджа
+const call = document.querySelector('.call');
+const btnsCall = document.querySelectorAll('.js-btn-call');
+
+btnsCall.forEach(btn => {
+  btn.addEventListener('click', () => {
+    call.classList.add('active');
+    body.classList.add('disabled-scroll');
+  });
+});
+
 // Отправить форму колл-пейджа
+const submitCall = document.querySelector('.js-call-submit');
+const callBlock = document.querySelector('.call-block');
+const callThanksBlock = document.querySelector('.call-thanks-block');
+
+submitCall.addEventListener('click', e => {
+  e.preventDefault();
+  callBlock.classList.add('hidden');
+  callThanksBlock.classList.add('active');
+  callThanksBlock.classList.remove('hidden');
+});
+
+// Закрытие колл-пейджа
+const callCloseBtn = document.querySelector('.js-call-close');
+const callThanksCloseBtn = document.querySelector('.js-call-thanks-close');
+
+callCloseBtn.addEventListener('click', () => {
+  call.classList.remove('active');
+  body.classList.remove('disabled-scroll');
+});
+
+callThanksCloseBtn.addEventListener('click', () => {
+  call.classList.remove('active');
+  body.classList.remove('disabled-scroll');
+
+  setTimeout(() => {
+    callBlock.classList.remove('hidden');
+    callThanksBlock.classList.add('hidden');
+    callThanksBlock.classList.remove('active');
+  }, 500);
+});
