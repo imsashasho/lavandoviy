1bcfd4cf5de4236cf473b1ed2b8684e8e856f2ff
Mazurchak Volodymyr <mazurchak.volodymyr.work@gmail.com>
2022-02-16 15:51:27 +0200

    Last adds to gallery pop-up

--- src/assets/scripts/gulp-modules/gallery.js
+++ src/assets/scripts/gulp-modules/gallery.js
@@ -1,12 +1,39 @@
 // Инициализируем слайдер
-const sliderConfig = {
+const sliderConfigTop = {
   speed: 400,
-  // autoHeight: true,
-  autoHeight: false,
+  autoHeight: true,
   slidesPerView: 1,
   freeMode: true,
   adaptiveHeight: true,
   allowTouchMove: true,
 };
 
-const swiper = new Swiper('.swiper', sliderConfig);
+const sliderConfigBottom = {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 'auto',
+  freeMode: true,
+  adaptiveHeight: true,
+  allowTouchMove: true,
+};
+
+const swiperGalleryTop = new Swiper('.swiper-gallery-top', sliderConfigTop);
+
+const swiperGalleryBottom = new Swiper('.swiper-gallery-bottom', sliderConfigBottom);
+
+// Вызов поп-апа
+const galleryFigures = document.querySelectorAll('.gallery__figure');
+const galleryPopUp = document.querySelector('.gallery-pop-up');
+const closePopUp = document.querySelector('.js-close-pop-up');
+
+galleryFigures.forEach(figure => {
+  figure.addEventListener('click', () => {
+    galleryPopUp.classList.add('active');
+    body.classList.add('disabled-scroll');
+  });
+});
+
+closePopUp.addEventListener('click', () => {
+  galleryPopUp.classList.remove('active');
+  body.classList.remove('disabled-scroll');
+});
